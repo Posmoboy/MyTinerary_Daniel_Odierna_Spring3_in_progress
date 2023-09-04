599d80aff2037cdb0c1bc1dd1f14826939053333
Posmoboy <posmoboy@gmail.com>
2023-09-04 16:55:27 -0300

    processenv

--- Back_MyTinerary_Daniel_Odierna/database.js
+++ Back_MyTinerary_Daniel_Odierna/database.js
@@ -7,7 +7,7 @@ const TEST_MONGO_URI = process.env['TEST_DATABASE_URL']
 
 
 
-mongoose.connect("mongodb+srv://MernTest:K2guMswWO1A8YbtX@cluster0.fg5gk5r.mongodb.net/NewCities")
+mongoose.connect(process.env['DATABASE_URL'])
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((error) => console.log("Database connection failed"));
 
